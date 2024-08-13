c930e972f3ea575a8acff536eb30b13e07889f12
ARCHERSPAPA <47944977+ARCHERSPAPA@users.noreply.github.com>
2024-08-13 14:23:22 +0800

    Update formatter.vue

--- docs/examples/input/formatter.vue
+++ docs/examples/input/formatter.vue
@@ -3,8 +3,8 @@
     v-model="input"
     style="width: 240px"
     placeholder="Please input"
-    :formatter="(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-    :parser="(value) => value.replace(/\$\s?|(,*)/g, '')"
+    :formatter="(value) => `# ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+    :parser="(value) => value.replace(/\#\s?|(,*)/g, '')"
   />
 </template>
 
